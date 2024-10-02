82888d2454eaf7e9f93a72d2809fcc236a430fca
Edgar <Meccu9i@gmail.com>
2024-10-02 14:59:56 +0300

    добавил View на страницу главная, добавил courses_item xml для recycler view, создал adapter viewholder , на странице авторизации добавил на телефоне что бы были только цифры , пароль скрытый , mail клавиатуру с @, добавил в профиле 3 Text_editor.

--- app/src/main/java/site/pnpl/igotit/view/home/HomeFragment.kt
+++ app/src/main/java/site/pnpl/igotit/view/home/HomeFragment.kt
@@ -1,20 +1,32 @@
 package site.pnpl.igotit.view.home
 
+import android.graphics.Color
 import androidx.fragment.app.viewModels
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import site.pnpl.igotit.R
+import android.widget.Toast
+import androidx.recyclerview.widget.LinearLayoutManager
+import site.pnpl.igotit.adapters.CoursesListRecyclerAdapter
+import site.pnpl.igotit.databinding.FragmentHomeBinding
+import site.pnpl.igotit.items.Courses
+import site.pnpl.igotit.view.MainActivity
 
 class HomeFragment : Fragment() {
-
     companion object {
         fun newInstance() = HomeFragment()
     }
-
     private val viewModel: HomeViewModel by viewModels()
+
+    private lateinit var binding: FragmentHomeBinding
+    private lateinit var coursesAdapter:CoursesListRecyclerAdapter
+
+    val coursesDataBase = listOf(
+        Courses("Выбрать обучение"),
+        Courses("Избранные курсы"),
+    )
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,6 +38,28 @@ class HomeFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        binding = FragmentHomeBinding.inflate(layoutInflater)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val courses_recycler = binding.coursesRecycler
+        //находим наш RV
+        courses_recycler.apply {
+            coursesAdapter= CoursesListRecyclerAdapter(object :CoursesListRecyclerAdapter.OnItemClickListener{
+                override fun click(courses: Courses) {
+                    Toast.makeText(context,"Тут будет что-то", Toast.LENGTH_SHORT).show()
+                }
+            })
+            //Присваиваем адаптер
+            adapter = coursesAdapter
+            //Присвои layoutmanager
+            layoutManager = LinearLayoutManager(requireContext())
+        }
+        //Кладем нашу БД в RV
+        coursesAdapter.addItems(coursesDataBase)
+
     }
 }
