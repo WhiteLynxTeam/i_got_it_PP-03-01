1b04a7b3e1a5a58298f270febb8b1f8c49035245
Edgar <Meccu9i@gmail.com>
2024-10-04 12:30:53 +0300

    Удалил рецыклер , холдеры, итем, переделал главную страницу, добавил фрагменты "выбрать обучение" и "избранные курсы", сделал переходы по ним по нажатию кнопок

---
+++ app/src/main/java/site/pnpl/igotit/view/home/HomeFavoritesCoursesFragment.kt
@@ -0,0 +1,20 @@
+package site.pnpl.igotit.view.home
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import site.pnpl.igotit.databinding.FragmentHomeFavoritesCoursesBinding
+
+class HomeFavoritesCoursesFragment : Fragment() {
+    private lateinit var binding:FragmentHomeFavoritesCoursesBinding
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentHomeFavoritesCoursesBinding.inflate(layoutInflater)
+        return binding.root
+
+    }
+}
--- app/src/main/java/site/pnpl/igotit/view/home/HomeFragment.kt
+++ app/src/main/java/site/pnpl/igotit/view/home/HomeFragment.kt
@@ -1,32 +1,23 @@
 package site.pnpl.igotit.view.home
 
-import android.graphics.Color
-import androidx.fragment.app.viewModels
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
-import androidx.recyclerview.widget.LinearLayoutManager
-import site.pnpl.igotit.adapters.CoursesListRecyclerAdapter
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
+import site.pnpl.igotit.R
 import site.pnpl.igotit.databinding.FragmentHomeBinding
-import site.pnpl.igotit.items.Courses
-import site.pnpl.igotit.view.MainActivity
 
 class HomeFragment : Fragment() {
     companion object {
         fun newInstance() = HomeFragment()
     }
+
     private val viewModel: HomeViewModel by viewModels()
 
     private lateinit var binding: FragmentHomeBinding
-    private lateinit var coursesAdapter:CoursesListRecyclerAdapter
-
-    val coursesDataBase = listOf(
-        Courses("Выбрать обучение"),
-        Courses("Избранные курсы"),
-    )
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -45,21 +36,15 @@ class HomeFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        val courses_recycler = binding.coursesRecycler
-        //находим наш RV
-        courses_recycler.apply {
-            coursesAdapter= CoursesListRecyclerAdapter(object :CoursesListRecyclerAdapter.OnItemClickListener{
-                override fun click(courses: Courses) {
-                    Toast.makeText(context,"Тут будет что-то", Toast.LENGTH_SHORT).show()
-                }
-            })
-            //Присваиваем адаптер
-            adapter = coursesAdapter
-            //Присвои layoutmanager
-            layoutManager = LinearLayoutManager(requireContext())
+        //переход во фрагмент с "выбрать обучение"
+        binding.chooseTrainingButton.setOnClickListener {
+            findNavController().navigate(R.id.action_homeFragment_to_homeTrainingFragment)
         }
-        //Кладем нашу БД в RV
-        coursesAdapter.addItems(coursesDataBase)
 
+        //переход во фрагмент с "избранными курсами"
+        binding.favoritesCoursesButton.setOnClickListener {
+            findNavController().navigate(R.id.action_homeFragment_to_homeFavoritesCoursesFragment)
+        }
     }
+
 }
