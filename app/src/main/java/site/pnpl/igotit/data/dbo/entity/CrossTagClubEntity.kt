baade2023f382cbed83c6708ecb4b714a0197abf
Mea <maevski.ed@gmail.com>
2024-10-25 09:30:28 +0300

    fix. не добавил первичный ключ для кросс таблиц. исправил.

--- app/src/main/java/site/pnpl/igotit/data/dbo/entity/CrossTagClubEntity.kt
+++ app/src/main/java/site/pnpl/igotit/data/dbo/entity/CrossTagClubEntity.kt
@@ -4,7 +4,7 @@ import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
-@Entity(tableName = "CROSS_TAGS_CLUBS")
+@Entity(tableName = "CROSS_TAGS_CLUBS", primaryKeys = ["tagUuid", "clubUuid"])
 data class CrossTagClubEntity(
     @field: ColumnInfo(name = "tagUuid") val tagUuid: String,
     @field: ColumnInfo(name = "clubUuid") val clubUuid: String,
